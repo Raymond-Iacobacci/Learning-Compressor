9d98f6a0492e09be269711bf6303761c2f741393
Raymond-Iacobacci <66854622+Raymond-Iacobacci@users.noreply.github.com>
2021-09-24 23:32:20 -0700

    Update usemodel.py

--- usemodel.py
+++ usemodel.py
@@ -7,7 +7,6 @@ from keras.utils import np_utils
 
 #Read all corrections into file--whole filename must be specified at the beginning
 inputFileName = sys.argv[1]
-print(inputFileName+".corrections.csv")
 inputFile = open(inputFileName+".corrections.csv", 'r')
 csvreader = csv.reader(inputFile)
 vals = []
@@ -19,13 +18,6 @@ for i in range(3, len(vals)):
 initString = vals[0]
 n_vocab = int(vals[1])
 totalLength = int(vals[2])
-print(len(vals))
-print(initString)
-print(totalLength)
-
-
-
-#need to add in data length
 
 #Format data into model
 for i in range(totalLength - 100):
@@ -36,7 +28,6 @@ for i in range(totalLength - 100):
     x = x/float(n_vocab)
 chars = sorted(list(set(initString)))
 charToInt = dict((c, i) for i, c in enumerate(chars))
-#intToChar = dict((i, c) for i, c in enumerate(chars))
 pattern = [charToInt[char] for char in initString]
 x = numpy.reshape(pattern, (1, len(pattern), 1))
 x = x/float(n_vocab)
@@ -75,5 +66,3 @@ for i in range(100, totalLength):
 
 #CHECK
 #need to compare to regular accuracy of model
-print(len(recreation))
-print(recreation)
